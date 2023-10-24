c3c886e9b43d064876d85b7c6dc1c3f385a56645
hovhannisyandav@icloud.com <hovhannes@tigra.app>
2023-10-24 16:33:07 +0400

    Added "success" on res

--- src/controllers/user/index.ts
+++ src/controllers/user/index.ts
@@ -5,7 +5,10 @@ import { User } from "../../entities/User";
 const userRepository = AppDataSource.getRepository(User);
 
 export class UserController {
-  static async createUser(req: Request, res: Response<User | unknown>): Promise<void> {
+  static async createUser(
+    req: Request,
+    res: Response<User | unknown>
+  ): Promise<void> {
     try {
       const { id } = req.body.currentUser;
       const { name, surname, age } = req.body;
@@ -14,10 +17,16 @@ export class UserController {
 
       if (userInputValidation.isValid) {
         const user = new User();
-        Object.assign(user, { name, surname, age, createdById: id, createdDate: new Date() });
+        Object.assign(user, {
+          name,
+          surname,
+          age,
+          createdById: id,
+          createdDate: new Date(),
+        });
 
         await userRepository.save(user);
-        res.status(201).json(user);
+        res.status(201).json({ data: user, success: true });
       } else {
         res.status(400).json({ error: userInputValidation.errorMessage });
       }
@@ -43,7 +52,9 @@ export class UserController {
         }),
       ]);
 
-      res.status(200).json({ success: true, data: allUsers, count: allUsersCount });
+      res
+        .status(200)
+        .json({ success: true, data: allUsers, count: allUsersCount });
     } catch (error) {
       res.status(400).json({ error: `${error}` });
     }
@@ -60,7 +71,7 @@ export class UserController {
 
       if (userToRemove) {
         await userRepository.remove(userToRemove);
-        res.status(200).json(userToRemove);
+        res.status(200).json({ data: userToRemove, success: true });
       } else {
         res.status(404).json({ error: "User not found" });
       }
@@ -75,19 +86,24 @@ export class UserController {
       const { name, surname, age } = req.body;
 
       const userInputValidation = validateUserInput({ name, surname, age });
-  
+
       if (userInputValidation.isValid) {
         const userToEdit = await userRepository.findOneBy({
           id,
         });
-  
+
         if (userToEdit) {
-          Object.assign(userToEdit, { name, surname, age, lastModifiedDate: new Date() });
+          Object.assign(userToEdit, {
+            name,
+            surname,
+            age,
+            lastModifiedDate: new Date(),
+          });
 
           await userRepository.save(userToEdit);
           res.status(200).json({ success: true, data: userToEdit });
         } else {
-          res.status(404).json({ success: false, error: 'User not found' });
+          res.status(404).json({ success: false, error: "User not found" });
         }
       } else {
         res.status(404).json({ success: false, error: "User not found" });
@@ -109,10 +125,10 @@ export class UserController {
 
       const allUsers = await query.getMany();
 
-      const filteredUsers = allUsers.filter(user => {
+      const filteredUsers = allUsers.filter((user) => {
         if (!searchValue) return true;
 
-        const agePattern = new RegExp(searchValue, 'i');
+        const agePattern = new RegExp(searchValue, "i");
         return (
           user.name.includes(searchValue) ||
           user.surname.includes(searchValue) ||
@@ -121,7 +137,10 @@ export class UserController {
       });
 
       const allUsersCount = filteredUsers.length;
-      const paginatedUsers = filteredUsers.slice(startIndex, startIndex + limit);
+      const paginatedUsers = filteredUsers.slice(
+        startIndex,
+        startIndex + limit
+      );
 
       res.status(200).json({ data: paginatedUsers, count: allUsersCount });
     } catch (error) {
@@ -130,15 +149,21 @@ export class UserController {
   }
 }
 
-function validateUserInput(reqBody: any): { isValid: boolean; errorMessage?: string } {
+function validateUserInput(reqBody: any): {
+  isValid: boolean;
+  errorMessage?: string;
+} {
   const { name, surname, age } = reqBody;
 
   if (!name || !surname || age == null) {
-    return { isValid: false, errorMessage: 'Missing required field' };
+    return { isValid: false, errorMessage: "Missing required field" };
   }
 
-  if (typeof age !== 'number' || age < 1) {
-    return { isValid: false, errorMessage: 'Age must be a number greater than or equal to 1' };
+  if (typeof age !== "number" || age < 1) {
+    return {
+      isValid: false,
+      errorMessage: "Age must be a number greater than or equal to 1",
+    };
   }
 
   return { isValid: true };
